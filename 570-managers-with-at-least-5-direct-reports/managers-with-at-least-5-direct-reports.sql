4d8971c126c3d21756f271b0e1467e194acae1bb
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2026-09-21 14:19:00 -0400

    Time: 335 ms (32.11%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+with managers as (
+    select managerId, count(*) as count
+    from employee
+    where managerId is not null
+    group by managerId
+)
+
+select e.name
+from managers as m
+join employee as e
+on m.managerId = e.id
+where m.count >= 5
